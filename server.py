81fcb2fcd2a5863879fd4a4180405677668a2688
AmaseCocoa <amase.cocoa@gmail.com>
2024-08-28 01:24:48 +0900

    同上

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import traceback
 
 import aiofiles
 import aiohttp
@@ -41,70 +42,85 @@ async def proxy_image(request):
     preview = "preview" in query_params
     badge = "badge" in query_params
 
-    if not url:
-        return web.Response(status=400, text="Missing 'url' parameter")
-
     try:
-        url = urllib.parse.unquote(url)
-    except Exception as e:
-        return web.Response(status=400, text="Invalid 'url' parameter")
+        if not url:
+            return web.Response(status=400, text="Missing 'url' parameter")
 
-    async with aiohttp.ClientSession() as session:
-        image_data, content_type = await fetch_image(session, url)
+        try:
+            url = urllib.parse.unquote(url)
+        except Exception as e:
+            return web.Response(status=400, text="Invalid 'url' parameter")
 
-        if image_data is None:
-            if fallback:
+        async with aiohttp.ClientSession() as session:
+            image_data, content_type = await fetch_image(session, url)
+
+            if image_data is None:
+                if fallback:
+                    headers = {
+                        "Cache-Control": "max-age=300",
+                        "Content-Type": "image/webp",
+                        "Content-Security-Policy": "default-src 'none'; img-src 'self'; media-src 'self'; style-src 'unsafe-inline'",
+                        "Content-Disposition": "inline; filename=image.webp",
+                    }
+                    async with aiofiles.open("./assets/fallback.webp", "rb") as f:
+                        return web.Response(
+                            status=200, body=await f.read(), headers=headers
+                        )
+                return web.Response(status=404, text="Image not found")
+            if "image" not in content_type:
+                logger.info("Media is Not Image. Redirecting to Response...")
                 headers = {
-                    "Cache-Control": "max-age=300",
-                    "Content-Type": "image/webp",
+                    "Cache-Control": "max-age=31536000, immutable",
+                    "Content-Type": content_type,
                     "Content-Security-Policy": "default-src 'none'; img-src 'self'; media-src 'self'; style-src 'unsafe-inline'",
                     "Content-Disposition": "inline; filename=image.webp",
                 }
-                async with aiofiles.open("./assets/fallback.webp", "rb") as f:
-                    return web.Response(
-                        status=200, body=await f.read(), headers=headers
-                    )
-            return web.Response(status=404, text="Image not found")
-        if "image" not in content_type:
-            logger.info("Media is Not Image. Redirecting to Response...")
+                return web.Response(status=200, body=image_data, headers=headers)
+
+            image = Image.open(io.BytesIO(image_data))
+
+            if emoji:
+                image.thumbnail((128, 128))
+            elif avatar:
+                image.thumbnail((320, 320))
+            elif preview:
+                image.thumbnail((200, 200))
+            elif badge:
+                image = image.convert("RGBA")
+                image = image.resize((96, 96))
+
+            output = io.BytesIO()
+            image_format = "WEBP" if not badge else "PNG"
+            if image_format == "PNG":
+                image.save(output, format=image_format, optimize=True)
+            elif image_format == "WEBP":
+                image.save(output, format=image_format, quality=80)
+            output.seek(0)
+
             headers = {
-                "Cache-Control": "max-age=31536000, immutable",
-                "Content-Type": content_type,
+                "Cache-Control": "max-age=31536000, immutable"
+                if image_data
+                else "max-age=300",
+                "Content-Type": f"image/{image_format.lower()}",
+                "Content-Security-Policy": "default-src 'none'; img-src 'self'; media-src 'self'; style-src 'unsafe-inline'",
+                "Content-Disposition": f"inline; filename=image.{image_format.lower()}",
+            }
+
+            return web.Response(body=output.read(), headers=headers)
+    except Exception as e:
+        print(traceback.format_exc())
+        if fallback:
+            headers = {
+                "Cache-Control": "max-age=300",
+                "Content-Type": "image/webp",
                 "Content-Security-Policy": "default-src 'none'; img-src 'self'; media-src 'self'; style-src 'unsafe-inline'",
                 "Content-Disposition": "inline; filename=image.webp",
             }
-            return web.Response(status=200, body=image_data, headers=headers)
-
-        image = Image.open(io.BytesIO(image_data))
-
-        if emoji:
-            image.thumbnail((128, 128))
-        elif avatar:
-            image.thumbnail((320, 320))
-        elif preview:
-            image.thumbnail((200, 200))
-        elif badge:
-            image = image.convert("RGBA")
-            image = image.resize((96, 96))
-
-        output = io.BytesIO()
-        image_format = "WEBP" if not badge else "PNG"
-        if image_format == "PNG":
-            image.save(output, format=image_format, optimize=True)
-        elif image_format == "WEBP":
-            image.save(output, format=image_format, quality=80)
-        output.seek(0)
-
-        headers = {
-            "Cache-Control": "max-age=31536000, immutable"
-            if image_data
-            else "max-age=300",
-            "Content-Type": f"image/{image_format.lower()}",
-            "Content-Security-Policy": "default-src 'none'; img-src 'self'; media-src 'self'; style-src 'unsafe-inline'",
-            "Content-Disposition": f"inline; filename=image.{image_format.lower()}",
-        }
-
-        return web.Response(body=output.read(), headers=headers)
+            async with aiofiles.open("./assets/fallback.webp", "rb") as f:
+                return web.Response(
+                    status=200, body=await f.read(), headers=headers
+                )
+        return web.Response(status=404, text="Image not found")
 
 
 app = web.Application()
@@ -113,5 +129,5 @@ app.router.add_get("/proxy/{filename}", proxy_image)
 
 if __name__ == "__main__":
     web.run_app(
-        app, port=os.environ.get("PORT", 3003), host=os.environ.get("HOST", "0.0.0.0")
+        app, port=os.environ.get("PORT", 3004), host=os.environ.get("HOST", "0.0.0.0")
     )
